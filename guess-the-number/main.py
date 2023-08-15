51a8f74f6b0e04505253f81c222a2186da8ce6a7
adilyb <muhammedadilyb@gmail.com>
2023-08-15 23:42:59 +0530

    project no.3 guess the number

---
+++ guess-the-number/main.py
@@ -0,0 +1,12 @@
+import random
+
+number = random.randint(1,10)
+
+for i in range(0,3):
+    user = int(input("guess the number :"))
+    if user == number:
+        print('hurray!')
+        print(f'Your guess is right the number is {number}')
+        break
+    if user != number:
+        print(f'Your guess is incorrect the number is {number}')
